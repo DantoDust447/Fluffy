ddaca37fde0c0f3b9f2eec1210b70d98a584243f
DantoDust447 <dantejosesanchez724@gmail.com>
2025-06-02 08:32:56 -0600

    link de conexion válida

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="procesos_crud/conexion.php">Conexion</a>
+</body>
+</html>
