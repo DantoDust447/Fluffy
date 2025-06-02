b97a1dd7d074f90157acfb5a6170ec95c1e639fc
DantoDust447 <dantejosesanchez724@gmail.com>
2025-06-02 09:24:11 -0600

    TRabajen weobonez

--- index.php
+++ index.php
@@ -5,13 +5,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <header>
-        <h1>Ciudadanos</h1>
-    <a href="procesos_crud/conexion.php">Verificar conexion</a> |
-    <a href="tablas_de_la_db/ciudadanos.php">Ciudadanos</a>
+    <header class="bg-primary text-white p-3">
+        <div class="container">
+            <h1>Bienvenido a la gestión de Ciudadanos</h1>
+            <nav>
+                <a href="procesos_crud/conexion.php" class="text-white">Verificar conexión</a> |
+                <a href="tablas_de_la_db/ciudadanos.php" class="text-white">Ciudadanos</a>
+            </nav>
+        </div>
+    </header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js" integrity="sha384-RuyvpeZCxMJCqVUGFI0Do1mQrods/hhxYlcVfGPOfQtPJh0JCw12tUAZ/Mv10S7D" crossorigin="anonymous"></script>
 </body>
 </html>
--- tablas_de_la_db/ciudadanos.php
+++ tablas_de_la_db/ciudadanos.php
@@ -9,14 +9,17 @@ $ejecutar = mysqli_query($conexion, $sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/tables.css">
-    <link rel="stylesheet" href="../styles/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <title>Ciudadanos</title>
 </head>
 <body>
-    <header>
-        <a href="../index.php"><img src="../imgs/house.svg" alt="Inicio">_Inicio</a>
+    <header class="bg-primary text-white p-3">
+        <div class="container">
+            <h1>Ciudadanos</h1>
+            <nav>
+                <a href="../index.php"><button type="button" class="btn btn-secondary">Inicio</button></a>
+            </nav>
+        </div>
     </header>
     <nav>
         <button>Agregar ciudadano</button>
