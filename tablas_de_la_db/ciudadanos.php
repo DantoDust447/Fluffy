c2dc348dec2ddbb58e2916b1b00ad7b8e6e2ede4
DantoDust447 <dantejosesanchez724@gmail.com>
2025-06-02 09:15:41 -0600

    bootstrap css

--- tablas_de_la_db/ciudadanos.php
+++ tablas_de_la_db/ciudadanos.php
@@ -11,9 +11,15 @@ $ejecutar = mysqli_query($conexion, $sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/tables.css">
     <link rel="stylesheet" href="../styles/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <title>Ciudadanos</title>
 </head>
 <body>
-    <a href="../index.php"><img src="../imgs/house.svg" alt="Inicio">_Inicio</a>
+    <header>
+        <a href="../index.php"><img src="../imgs/house.svg" alt="Inicio">_Inicio</a>
+    </header>
+    <nav>
+        <button>Agregar ciudadano</button>
+    </nav>
 </body>
 </html>
